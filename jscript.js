0ff241163af9a0e0a50ac18131469205540cc529
umaibal <31415370+umaibal@users.noreply.github.com>
2018-01-05 00:01:25 -0500

    Version 1.0 of website

---
+++ jscript.js
@@ -0,0 +1,14 @@
+//use javascript for the sidebar to have
+//reactions based on user interaction
+
+
+//function to open sidebar once hamburger is clicked
+function toggleSidebar() {
+document.getElementById('sidebar').classList.toggle('active');
+}
+
+//function to close the sidebar after opened by user
+function close() {
+document.getElementById('sidebar').style.display = "none";
+
+}
